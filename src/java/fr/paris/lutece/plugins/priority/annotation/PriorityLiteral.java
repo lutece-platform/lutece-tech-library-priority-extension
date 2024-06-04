8c5c1f05c1b8052124d4026e96870353795e4036
evdngsl <158455076+evdngsl@users.noreply.github.com>
2024-06-04 14:23:39 +0200

    LUT-28096 : Priority-extension to manage alternatives and @Priority value from configuration sources.

---
+++ src/java/fr/paris/lutece/plugins/priority/annotation/PriorityLiteral.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2002-2024, City of Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.priority.annotation;
+
+import jakarta.annotation.Priority;
+import jakarta.enterprise.util.AnnotationLiteral;
+
+/**
+ * Allows the instantiation of @Priority annotation programmatically.
+ */
+public class PriorityLiteral extends AnnotationLiteral<Priority> implements Priority
+{
+    private static final long serialVersionUID = 1L;
+
+    private final int _value;
+
+    @Override
+    public int value( )
+    {
+        return _value;
+    }
+
+    public static PriorityLiteral of( int value )
+    {
+        return new PriorityLiteral( value );
+    }
+
+    private PriorityLiteral( int value )
+    {
+        this._value = value;
+    }
+
+}
